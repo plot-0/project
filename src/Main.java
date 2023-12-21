e94253e557db4d3802c36e039f2604e42bfaa0b7
mzk <2069276944@qq.com>
2023-12-21 20:36:34 +0800

    加入bgm

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import controller.AudioFilePlayer;
 import controller.GameController;
 import view.GameFrame;
 import view.Menu;
 
 import javax.swing.*;
 import view.JTextFieldFrame;
+import controller.AudioPlayer;
+import controller.AudioFilePlayer;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             Menu menu = new Menu(1100,810);
             menu.setVisible(true);
+            AudioPlayer.playBgm("resource/bgm.wav");
         });
     }
 }
